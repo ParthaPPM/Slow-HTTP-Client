93a67314022a03fc510e3dc62e75f79f215c690a
Partha Pratim Manna <partha_pratim_manna@yahoo.in>
2022-12-29 19:16:56 +0530

    Javadoc comments added to HttpClient and HttpsClient classes

--- lib/src/main/java/io/github/parthappm/http/client/HttpClient.java
+++ lib/src/main/java/io/github/parthappm/http/client/HttpClient.java
@@ -3,13 +3,25 @@ package io.github.parthappm.http.client;
 import java.io.IOException;
 import java.net.Socket;
 
+/**
+ * The HTTP Client implementation for non-secured HTTP connection.
+ */
 public class HttpClient extends Client
 {
+	/**
+	 * Creates a socket connection to the specified host at port 80 and constructs a new HttpClient object.
+	 * @param host The host name or ip address of the server
+	 */
 	public HttpClient(String host)
 	{
 		this(host, 80);
 	}
 
+	/**
+	 * Creates a socket connection to the specified host at specified port and constructs a new HttpClient object.
+	 * @param host The host name or ip address of the server
+	 * @param port The port number at which the server is running
+	 */
 	public HttpClient(String host, int port)
 	{
 		try
--- lib/src/main/java/io/github/parthappm/http/client/HttpsClient.java
+++ lib/src/main/java/io/github/parthappm/http/client/HttpsClient.java
@@ -6,13 +6,25 @@ import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
 import java.net.Socket;
 
+/**
+ * The HTTP Client implementation for secured HTTP connection over Secured Sockets Layer (SSL)
+ */
 public class HttpsClient extends Client
 {
+	/**
+	 * Creates a socket connection to the specified host at port 443 and constructs a new HttpsClient object.
+	 * @param host The host name or ip address of the server
+	 */
 	public HttpsClient(String host)
 	{
 		this(host, 443);
 	}
 
+	/**
+	 * Creates a socket connection to the specified host at specified port and constructs a new HttpsClient object.
+	 * @param host The host name or ip address of the server
+	 * @param port The port number at which the server is running
+	 */
 	public HttpsClient(String host, int port)
 	{
 		try
