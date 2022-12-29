77425be36b0a0d1586c8419ae473d08bba13fe9e
Partha Pratim Manna <partha_pratim_manna@yahoo.in>
2022-12-29 20:01:57 +0530

    Javadoc comments added to Response class

--- lib/src/main/java/io/github/parthappm/http/client/Response.java
+++ lib/src/main/java/io/github/parthappm/http/client/Response.java
@@ -3,6 +3,9 @@ package io.github.parthappm.http.client;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+/**
+ * The Response class contains all the data related to HTTP response returned by the server
+ */
 public class Response
 {
 	private final String version;
@@ -25,31 +28,55 @@ public class Response
 		this.body = body;
 	}
 
+	/**
+	 * Getter method to get the HTTP version specified by the server in the response.
+	 * @return The HTTP version specified by the server in the response
+	 */
 	public String getVersion()
 	{
 		return version;
 	}
 
+	/**
+	 * Getter method to get the response status code.
+	 * @return The response status code
+	 */
 	public int getStatusCode()
 	{
 		return statusCode;
 	}
 
+	/**
+	 * Getter method to get the response status text.
+	 * @return The response status text
+	 */
 	public String getStatusText()
 	{
 		return statusText;
 	}
 
+	/**
+	 * Getter method to get the response headers.
+	 * @return The response headers
+	 */
 	public Map<String, String> getHeaders()
 	{
 		return headers;
 	}
 
+	/**
+	 * Getter method to get the response body as bytes array.
+	 * @return The response body as byte array
+	 */
 	public byte[] getBody()
 	{
 		return body;
 	}
 
+	/**
+	 * Getter method to get the response body as String representation decoded by UTF 8 standard.
+	 * @return The response body as String representation decoded by UTF 8 standard
+	 */
 	public String getText()
 	{
 		return new String(body, StandardCharsets.UTF_8);
